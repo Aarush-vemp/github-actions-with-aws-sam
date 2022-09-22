406a000e2967e73c08b369f77403b5818f501884
Aarush Sharma <aarushsharma@virtualemployee.com>
2022-09-22 17:08:05 +0530

    new code update

--- handler.js
+++ handler.js
@@ -1,18 +1,16 @@
 "use strict";
 
 module.exports.hello = async (event) => {
-  let randomNumber = Math.floor(Math.random() * 10)
-  return randomNumber;
-  // return {
-    // statusCode: 200,
-    // body: JSON.stringify(
-    //   {
-    //     message: "configured new keys for user_two",
-    //     input: event,
-    //   },
-    //   null,
-    //   2
-    // ),
+  
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "configured new keys for user_two",
+      },
+      null,
+      2
+    ),
     
-  // };
+  };
 };
